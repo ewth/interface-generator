26182cbb039e0e514b9bebe932ec167d85ace1f7
Ewan Thompson <ewan.thompson@thequotecompany.com.au>
2019-04-24 16:53:00 +0800

    Revert to original CardDetails class

--- example/CardDetails.php
+++ example/CardDetails.php
@@ -10,7 +10,7 @@ namespace Eway\Rapid\Model;
 /**
  * Class CardDetails.
  *
- * @property string $SOMEBodyName        Name on the card
+ * @property string $Name        Name on the card
  * @property string $Number      Credit card number (16-21 digits plaintext, Up to 512 chars for eCrypted values)
  * @property string $Expiry_month 2 Digits
  * @property string $ExpiryYear  2 or 4 digits e.g. "15" or "2015"
